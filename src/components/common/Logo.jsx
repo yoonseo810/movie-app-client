40404cba2e2d11d72d4f140c904ac39dd999fe1e
yoonseo810 <85579849+yoonseo810@users.noreply.github.com>
2023-02-02 15:14:19 -0500

    first commit

---
+++ src/components/common/Logo.jsx
@@ -0,0 +1,14 @@
+import { Typography, useTheme } from '@mui/material';
+
+const Logo = () => {
+  const theme = useTheme();
+
+  return (
+    <Typography fontWeight="700" fontSize="1.7rem">
+      TM<span style={{ color: theme.palette.primary.main }}>DB</span>
+      {/* <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_1-8ba2ac31f354005783fab473602c34c3f4fd207150182061e425d366e4f34596.svg" alt="TMDB LOGO" /> */}
+    </Typography>
+  );
+};
+
+export default Logo;
